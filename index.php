c9ef8bf4bc87d32348beef257c20e8ac527f9ae1
skeito <s.keito896@gmail.com>
2024-04-02 01:13:35 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+$err_msg = '';
+if (isset($_POST['login'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    try {
+        $db = new PDO('mysql:host=localhost;dbname=sample', 'keito', 'shitara@1324');
+        $sql = 'select * from users where username=? and password=?';
+        $stmt = $db->prepare($sql);
+        $stmt->execute(array($username, $password));
+        $result = $stmt->fetch();
+        $stmt = null;
+        $db = null;
+
+        if ($result) {
+            header('Location: http://localhost:8080/home.php');
+            exit;
+        } else {
+            $err_msg = "ユーザ名またはパスワードが誤りです。";
+        }
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+        exit;
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>ログイン画面</title>
+    </head>
+    <body>
+        <h1>ログイン画面</h1>
+        <form action="" method="POST">
+            <?php if ($err_msg !== null && $err_msg !== '') { echo $err_msg . "<br />"; } ?>
+            ユーザ名<input type="text" name="username" value="" /><br />
+            パスワード<input type="password" name="password" value="" /><br />
+            <input type="submit" name="login" value="ログイン">
+        </form>
+        <a href="signin.php">新規登録</a>
+    </body>
+</html>
---
+++ signin.php
@@ -0,0 +1,37 @@
+<?php
+
+if (isset($_POST['signin'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    try {
+        $db = new PDO('mysql:host=localhost;dbname=sample', 'keito', 'shitara@1324');
+        $sql = 'insert into users(username, password) values(?, ?)';
+        $stmt = $db->prepare($sql);
+        $stmt->execute(array($username, $password));
+        $stmt = null;
+        $db = null;
+        
+        header('Location: http://localhost:8080/index.php');
+        exit;
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+        exit;
+    }
+}
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>新規登録画面</title>
+    </head>
+    <body>
+        <h1>新規登録画面</h1>
+        <form action="" method="POST">
+            ユーザ名<input type="text" name="username" value="" /><br />
+            パスワード<input type="password" name="password" value="" /><br />
+            <input type="submit" name="signin" value="新規登録">
+        </form>
+    </body>
+</html>
